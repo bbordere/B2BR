c21b5d4985d9fa5b4c4dfa4928086b0b35e6ace9
Bastien B <45495330+bbordere@users.noreply.github.com>
2021-12-21 21:11:11 +0100

    update

--- monitoring.sh
+++ monitoring.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 vCPU=$(grep -c processor /proc/cpuinfo)
+
 PCPU=$(grep "physical id" /proc/cpuinfo | uniq |wc -l)
+
 ARCH=$(uname -a)
+
 TCP=$(cat /proc/net/sockstat | awk '$1 == "TCP:" {printf $3}')
-IP=$(hostname -i)
+
+IP=$(hostname -I)
+
 U_RAM=$(free -m | awk '$1 == "Mem:" {print $3}')
 T_RAM=$(free -m | awk '$1 == "Mem:" {print $2}')
 P_RAM=$((100 * U_RAM / T_RAM))
+
 UC=$(users | wc -l)
+
 CPU_L=$(top -bn1 |grep load |awk '{print $12}')
 CPU_L=${CPU_L%,}
-U_DISK=$(df -h '/' | awk 'NR==2 {print $3}')
-U_DISK=${U_DISK%G}
-T_DISK=$(df -h '/' | awk 'NR==2 {print $2}')
-P_DISK=$(df -h '/' | awk 'NR==2 {print $5}')
+
+U_DISK=$(df -m --output=used --total | tail -1 |cut -c 4-)
+T_DISK=$(df -m --output=size --total | tail -1 |cut -c 4-)
+P_DISK=$((100 * U_DISK / T_DISK))
+
 L_BOOT=$(who -b | awk '{$1=$2="";print $0}' | cut -d " " -f3-)
+
 LVM_S=$(lsblk | grep "lvm" | wc -l)
 LVM_A=$(if [ $lvmt -eq 0 ]; then echo no; else echo yes; fi)
+
 SUDO_C=$(journalctl _COMM=sudo -q | grep COMMAND | wc -l)
+
 MAC=$(ip link show | awk '$1 == "link/ether" {print $2}' | awk 'NR==1{print $1}')
 
 echo  "#Architecture : $ARCH"
